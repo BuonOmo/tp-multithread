87411dc3ea57e1c9ebd03481980ba41cba9137f6
Kaisercroc <pierre.bayle@insa-lyon.fr>
2016-03-18 18:01:29 +0100

    Ajout des phases pour la mère

--- src/Mere.cpp
+++ src/Mere.cpp
@@ -6,11 +6,14 @@
     e-mail               : pbayle@insa-lyon.fr
 *************************************************************************/
 
-//---------- Réalisation du module <Mere> (fichier Mere.cpp) -----
+//---------- Réalisation du main <Mere> (fichier Mere.cpp) -----
 
 /////////////////////////////////////////////////////////////////  INCLUDE
 //-------------------------------------------------------- Include système
 #include <unistd.h>
+#include <iostream>
+#include <sys/types.h>
+#include <sys/ipc.h>
 
 //------------------------------------------------------ Include personnel
 #include "Menu.h"
@@ -23,6 +26,25 @@
 //------------------------------------------------------------------ Types
 
 //---------------------------------------------------- Variables statiques
+static pid_t clavier;
+static pid_t entreeBPP;
+static pid_t entreeBPA;
+static pid_t entreeGB;
+static pid_t sortie;
+
+static int baLEntreeBPP;
+static int baLEntreeBPA;
+static int baLEntreeGB;
+static int baLSortie;
+
+static int mpEntreeBPP;
+static int mpEntreeBPA;
+static int mpEntreeGB;
+static int mpSortie;
+
+void Initialisation();
+void Moteur();
+void Destruction();
 
 //------------------------------------------------------ Fonctions privées
 //static type nom ( liste de paramètres )
@@ -37,44 +59,106 @@
 
 //////////////////////////////////////////////////////////////////  PUBLIC
 //---------------------------------------------------- Fonctions publiques
-int main ()
+
+void Initialisation ()
 {
-	pid_t nofils;
-	pid_t noE1;
-	pid_t noE2;
-	pid_t noE3;
-	pid_t noS;
 	
-	if((nofils = fork()) == 0)
+	key_t cleEntreeBPP = ftok("balBPP",1);
+	key_t cleEntreeBPA = ftok("balBPA",1);
+	key_t cleEntreeGB = ftok("balGB",1); 
+	key_t cleSortie = ftok("balS",1);
+	
+	/** Création des BaL **/
+	if (baLEntreeBPP = msgget(cleEntreeBPP, IPC_CREAT) == -1)
+	{
+		cout << "Erreur pour la clé BPP (BAL)" << endl;
+	}	
+	if (baLEntreeBPA = msgget(cleEntreeBPA, IPC_CREAT)
+	{
+		cout << "Erreur pour la clé BPA (BAL)" << endl;
+	}	
+	if (baLEntreeGB = msgget(cleEntreeGB, IPC_CREAT)
+	{
+		cout << "Erreur pour la clé GB (BAL)" << endl;
+	}
+	if (baLSortie = msgget(cleSortie, IPC_CREAT)
+	{
+		cout << "Erreur pour la clé Sortie (BAL)" << endl;
+	}
+	/** Fin création des BaL **/
+	
+	/** Création des MP **/
+	if (baLEntreeBPP = msgget(cleEntreeBPP, IPC_CREAT) == -1)
+	{
+		cout << "Erreur pour la clé BPP (MP)" << endl;
+	}	
+	if (baLEntreeBPA = msgget(cleEntreeBPA, IPC_CREAT)
+	{
+		cout << "Erreur pour la clé BPA (MP)" << endl;
+	}	
+	if (baLEntreeGB = msgget(cleEntreeGB, IPC_CREAT)
+	{
+		cout << "Erreur pour la clé GB (MP)" << endl;
+	}
+	if (baLSortie = msgget(cleSortie, IPC_CREAT)
+	{
+		cout << "Erreur pour la clé Sortie (MP)" << endl;
+	}
+	/** Fin création des MP **/
+	
+	if ((clavier = fork()) == 0)
 	{
 		SimulationClavier();
 	}
-	else if((noE1 = fork()) == 0)
-	{
-		BarriereEntree();
-	}
-	else if((noE2 = fork()) == 0)
-	{
-		BarriereEntree();
-	}
-	else if((noE3 = fork()) == 0)
-	{
-		BarriereEntree();
-	}
-	else if((noS = fork()) == 0)
-	{
-		BarriereSortie();
-	}
 	else
 	{
-		InitialiserApplication(XTERM);
-		sleep(10);
-		TerminerApplication();
+		if ((entreeBPP = fork()) == 0)
+		{
+			
+		}
+		if ((entreeBPA = fork()) == 0)
+		{
+			
+		}
+		if ((entreeGB = fork()) == 0)
+		{
+			
+		}
+		if ((sortie = fork()) == 0)
+		{
+			
+		}		
 	}
+}
+
+void Moteur ()
+{
 	
-	InitialiserApplication(XTERM);
-	sleep(10);
-	TerminerApplication();
+}
+
+void Destruction ()
+{
+	kill(entreeBPP, SIGUSR2);
+	kill(entreeBPA, SIGUSR2);
+	kill(entreeGB, SIGUSR2);
+	kill(sortie, SIGUSR2);
+	kill(clavier, SIGUSR2);
+	msgctl(baLEntreeBPP,IPC_RMID,0);
+	msgctl(baLEntreeBPA,IPC_RMID,0);
+	msgctl(baLEntreeGB,IPC_RMID,0);
+	msgctl(baLSortie,IPC_RMID,0);
+	shmctl(baLEntreeBPP,IPC_RMID,0);
+	shmctl(baLEntreeBPA,IPC_RMID,0);
+	shmctl(baLEntreeGB,IPC_RMID,0);
+	shmctl(baLSortie,IPC_RMID,0);
+}
+
+int main ()
+{
+	Initialisation();
+	Moteur():
+	Destruction();
+	
 	return 0;
 
 }
